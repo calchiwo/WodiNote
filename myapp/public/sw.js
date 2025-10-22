55e228fe22769569dc712c9e30f2f9a5e85a23f0
Calchiwo <calebwodi33@gmail.com>
2025-10-22 18:19:09 +0100

    fix

--- myapp/public/sw.js
+++ myapp/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "wodinote-v1"
+const CACHE_NAME = "wodinote-v1.1"
 const STATIC_ASSETS = ["/", "/index.html", "/manifest.json", "/wodi-logo.jpg"]
 
 // Install event - cache static assets
